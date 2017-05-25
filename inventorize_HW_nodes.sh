806d5d09b22e774fce8365fba7742e3b71f31fee
alexander.baklanov <alexander.baklanov@megafon.ru>
2017-05-25 11:42:45 +0500

    Update inventorize_HW_nodes.sh

--- inventorize_HW_nodes.sh
+++ inventorize_HW_nodes.sh
@@ -3,9 +3,9 @@ echo "node name, bios version, bios release date, hdd controller fw version, net
 for i in $(cat nodes);
 do
     if ! (( ssh -o "StrictHostKeyChecking no" alexz0nder@$i "lscpu | grep Hypervisor" )); then
-        scp -o "StrictHostKeyChecking no" ~/firmware_versions.sh $i:/home/alexz0nder/firmware_versions.sh
-        ssh -o "StrictHostKeyChecking no" alexz0nder@$i "sudo chmod +x ./firmware_versions.sh"
-        ssh -o "StrictHostKeyChecking no" alexz0nder@$i "sudo ./firmware_versions.sh" | tee -a inventory.log
-        ssh -o "StrictHostKeyChecking no" alexz0nder@$i "sudo rm ./firmware_versions.sh"
+        scp -o "StrictHostKeyChecking no" ~/collect_firmwares.sh $i:/home/alexz0nder/collect_firmwares.sh
+        ssh -o "StrictHostKeyChecking no" alexz0nder@$i "sudo chmod +x ./collect_firmwares.sh"
+        ssh -o "StrictHostKeyChecking no" alexz0nder@$i "sudo ./collect_firmwares.sh" | tee -a inventory.log
+        ssh -o "StrictHostKeyChecking no" alexz0nder@$i "sudo rm ./collect_firmwares.sh"
     fi
 done
